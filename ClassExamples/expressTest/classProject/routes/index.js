10a5c451f19a42fb40c444c601eeb5117482ad67
Michael Dao <michaeltuandao@gmail.com>
2019-05-31 15:50:46 -0500

    added more function

--- ClassExamples/expressTest/classProject/routes/index.js
+++ ClassExamples/expressTest/classProject/routes/index.js
@@ -1,8 +1,10 @@
 let express = require('express');//SETUP
 let router = express.Router();//SETUP
+let dataFile = require('../')
 
 router.get('/', (req, res)=>{//SETUP
     res.send('homepage');//SETUP
 })
+
 
 module.exports = router;//SETUP
