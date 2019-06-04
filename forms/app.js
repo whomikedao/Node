e74611e591afe28555cc35bcc6b881ba796fc7e5
Michael Dao <michaeltuandao@gmail.com>
2019-06-04 09:58:30 -0500

    added body parser for post form

---
+++ forms/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+let port = 3000;
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(require('./routes/index'))
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
--- forms/routes/index.js
+++ forms/routes/index.js
@@ -1,3 +1,15 @@
 var express = require('express');
 var router = express.Router();
+let bodyParser = require('body-parser');
 
+router.get('/', (req, res)=>{
+
+    res.render('index');
+})
+
+router.post('/', (req, res)=>{
+    console.log('inside of post');
+    res.send('inside of post');
+}) 
+
+module.exports = router;
