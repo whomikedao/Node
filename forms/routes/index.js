64c13a9ac70bce6d5055f48d4ab4e356150724e6
Michael Dao <michaeltuandao@gmail.com>
2019-06-04 10:39:28 -0500

    made a post and then function

--- forms/routes/index.js
+++ forms/routes/index.js
@@ -5,11 +5,14 @@ let bodyParser = require('body-parser');
 router.get('/', (req, res)=>{
 
     res.render('index');
-})
+});
+
+router.use(bodyParser.urlencoded({extended: false}));
 
 router.post('/', (req, res)=>{
-    console.log('inside of post');
-    res.send('inside of post');
-}) 
+    console.log(`name: ${req.body.name} email: ${req.body.email} number: ${req.body.number}`);
+    console.log(req.body);
+    // res.send('inside of post');
+});
 
 module.exports = router;
